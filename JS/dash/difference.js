205193c33a213ffb571e2314c31daa071406513e
niangaopig <wangkaibro@gmail.com>
2022-02-02 17:15:17 +0800

    answer

---
+++ JS/dash/difference.js
@@ -0,0 +1,13 @@
+/**
+ * 创建一个具有唯一array值的数组，每个值不包含在其他给定的数组中。
+ * （注：即创建一个新数组，这个数组中的值，为第一个数字（array 参数）排除了给定数组中的值。）
+ * 该方法使用SameValueZero做相等比较。结果值的顺序是由第一个数组中的顺序确定。
+ * @param {array} array :  要检查的数组。
+ * @param {...array} values : 排除的值。
+ * @return {array} :返回一个过滤值后的新数组。
+ */
+
+function difference(array, values) {
+    
+    
+}
